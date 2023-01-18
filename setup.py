e06daf9017497d327e490b819d7b6a2f193c1dba
bhattaraiprayag <bhattaraiprayag@hotmail.com>
2023-01-18 21:47:46 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Building a recommender system based on the Book-Crossing data set',
+    author='Team 4',
+    license='MIT',
+)
